0a63baf29b809cc384c8a9e4fa1914a1d7cd5636
princethk <princethk787@gmail.com>
2024-04-11 01:00:40 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const moviesRoutes = require('./routes/movies');
+const tvSeriesRoutes = require('./routes/tvSeries');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+ mongoose.connect('mongodb://localhost:27017/entertainment');
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
+
+// Routes
+app.use('/api/movies', moviesRoutes);
+app.use('/api/tv-series', tvSeriesRoutes);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
